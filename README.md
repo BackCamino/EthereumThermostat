2b78cca88b6b642197a7838ff2da32d18fca4078
Marco Caminonni <mcaminonni@gmail.com>
2020-04-29 18:50:51 +0200

    updates

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Baccalà
+Via
--- Thermostat.sol
+++ Thermostat.sol
@@ -1,9 +1,11 @@
 pragma solidity^0.6.1;
 
 import './Sensor.sol';
+import './Heater.sol';
 
 
 contract Thermostat{
+    Heater private heater;
     Sensor private sensor;
     address owner;
     constructor() public{
@@ -11,16 +13,24 @@ contract Thermostat{
     }
     
     modifier onlySensor(){
-        require(msg.sender==address(sensor),"ciao");
+        require(msg.sender==address(sensor),"Only Sensor can do this function");
         _;
     }
+
     modifier onlyThermostat(){
-        require(msg.sender==owner,"ciao");
+        require(msg.sender==owner,"Only Thermostat can do this function");
         _;
     }
+
+    modifier onlyHeater(){
+        require(msg.sender==address(heater),"Only Heater can do this function");
+        _;
+    }
+
     function setSensor(address sensorAddress) public onlyThermostat {
         sensor=Sensor(sensorAddress);
     }
+
     function getSensorTem() public view returns(int16){
         return sensor.getTemperature();
     }
