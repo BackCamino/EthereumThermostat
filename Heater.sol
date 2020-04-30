d52cc820f30eebe55fe0af9ae346900a583948a6
Marco Caminonni <mcaminonni@gmail.com>
2020-04-30 02:44:29 +0200

    updates

--- Heater.sol
+++ Heater.sol
@@ -1,8 +1,31 @@
 pragma solidity^0.6.1;
 
-contract Heater{
-    constructor()public{
+import "./Status.sol";
 
+contract Heater{
+    Status status;
+    address owner;
+
+    constructor()public{
+        status=Status.OFF;
+        owner=msg.sender;
+    }
+
+    modifier onlyHeater(){
+        require(owner==msg.sender,"Only sensor can call this function");
+        _;
+    }
+    
+    function setON() public onlyHeater{
+        status=Status.ON;
+    }
+
+    function setOFF() public onlyHeater{
+        status=Status.OFF;
+    }
+    
+    function getStatus() public view onlyHeater returns(Status){
+        return status;
     }
     
 }
--- Thermostat.sol
+++ Thermostat.sol
@@ -2,16 +2,25 @@ pragma solidity^0.6.1;
 
 import './Sensor.sol';
 import './Heater.sol';
-
+import './Status.sol';
 
 contract Thermostat{
+
     Heater private heater;
     Sensor private sensor;
+    Status status;
     address owner;
+    
     constructor() public{
+        status=Status.OFF;
         owner=msg.sender;
     }
     
+    modifier isOn(){
+        require(status==Status.ON,"the devise must be on");
+        _;
+    }
+
     modifier onlySensor(){
         require(msg.sender==address(sensor),"Only Sensor can do this function");
         _;
@@ -28,10 +37,19 @@ contract Thermostat{
     }
 
     function setSensor(address sensorAddress) public onlyThermostat {
-        sensor=Sensor(sensorAddress);
+        
+    }
+
+    function setHeater(address sensorAddress) public onlyThermostat{
+        
+    }
+
+    function setON() public{
+        status=Status.ON;
     }
 
     function getSensorTem() public view returns(int16){
+        //Non funziona 
         return sensor.getTemperature();
     }
 }
